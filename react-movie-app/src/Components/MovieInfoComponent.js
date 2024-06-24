ff5529f39516a71bf923078bc1d8d38651f923be
root <root@ip-172-31-47-60.ap-south-1.compute.internal>
2024-06-24 09:23:58 +0000

    syntax error removed in MovieInfoComponent.js

--- react-movie-app/src/Components/MovieInfoComponent.js
+++ react-movie-app/src/Components/MovieInfoComponent.js
@@ -1,13 +1,8 @@
-import React, {
-	useState,
-	useEffect
-} from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 import Axios from 'axios';
 
-import {
-	API_KEY
-} from "../App";
+import { API_KEY } from "../App";
 
 const Container = styled.div`
   display: flex;
@@ -67,98 +62,63 @@ const Close = styled.span`
   opacity: 0.8;
 `;
 
-
 const MovieInfoComponent = (props) => {
+    const [movieInfo, setMovieInfo] = useState();
 
-	const [movieInfo, setMovieInfo] = useState();
+    const { selectedMovie } = props;
 
-	const {
-		selectedMovie
-	} = props;
+    useEffect(() => {
+        Axios.get(
+            `https://www.omdbapi.com/?i=${selectedMovie}&apikey=${API_KEY}`,
+        ).then((response) => setMovieInfo(response.data));
+    }, [selectedMovie]);
 
-	useEffect(() => {
-		Axios.get(
-			`https://www.omdbapi.com/?i=${selectedMovie}&apikey=${API_KEY}`,
-		).then((response) => setMovieInfo(response.data));
-	}, [selectedMovie]);
-
-	return ( < Container > {
-			movieInfo ? ( <
-				>
-				<
-				CoverImage src = {
-					movieInfo?.Poster
-				}
-				alt = "poster" / >
-				<
-				InfoColumn >
-				<
-				MovieName > {
-					movieInfo?.Type
-				}: < span > {
-					movieInfo?.Title
-				} < /span>  <
-				/MovieName> <
-				MovieInfo >
-				IMDB Rating: < span > {
-					movieInfo?.imdbRating
-				} < /span>  <
-				/MovieInfo>  <
-				MovieInfo >
-				Year: < span > {
-					movieInfo?.Year
-				} < /span>  <
-				/MovieInfo>  <
-				MovieInfo >
-				Language: < span > {
-					movieInfo?.Language
-				} < /span>  <
-				/MovieInfo>  <
-				MovieInfo >
-				Rated: < span > {
-					movieInfo?.Rated
-				} < /span> < /
-				MovieInfo > <
-				MovieInfo >
-				Released: < span > {
-					movieInfo?.Released
-				} < /span> < /
-				MovieInfo > <
-				MovieInfo >
-				Runtime: < span > {
-					movieInfo?.Runtime
-				} < /span> < /
-				MovieInfo > <
-				MovieInfo >
-				Genre: < span > {
-					movieInfo?.Genre
-				} < /span> < /
-				MovieInfo > <
-				MovieInfo >
-				Director: < span > {
-					movieInfo?.Director
-				} < /span> < /
-				MovieInfo > <
-				MovieInfo >
-				Actors: < span > {
-					movieInfo?.Actors
-				} < /span> < /
-				MovieInfo > <
-				MovieInfo >
-				Plot: < span > {
-					movieInfo?.Plot
-				} < /span> < /
-				MovieInfo > <
-				/InfoColumn> <
-				Close onClick = {
-					() => props.onMovieSelect()
-				} > X < /Close> < /
-				>
-			)
-				
-		} <
-		/Container>
-	)
+    return (
+        <Container>
+            {movieInfo ? (
+                <>
+                    <CoverImage src={movieInfo?.Poster} alt="poster" />
+                    <InfoColumn>
+                        <MovieName>
+                            {movieInfo?.Type}: <span>{movieInfo?.Title}</span>
+                        </MovieName>
+                        <MovieInfo>
+                            IMDB Rating: <span>{movieInfo?.imdbRating}</span>
+                        </MovieInfo>
+                        <MovieInfo>
+                            Year: <span>{movieInfo?.Year}</span>
+                        </MovieInfo>
+                        <MovieInfo>
+                            Language: <span>{movieInfo?.Language}</span>
+                        </MovieInfo>
+                        <MovieInfo>
+                            Rated: <span>{movieInfo?.Rated}</span>
+                        </MovieInfo>
+                        <MovieInfo>
+                            Released: <span>{movieInfo?.Released}</span>
+                        </MovieInfo>
+                        <MovieInfo>
+                            Runtime: <span>{movieInfo?.Runtime}</span>
+                        </MovieInfo>
+                        <MovieInfo>
+                            Genre: <span>{movieInfo?.Genre}</span>
+                        </MovieInfo>
+                        <MovieInfo>
+                            Director: <span>{movieInfo?.Director}</span>
+                        </MovieInfo>
+                        <MovieInfo>
+                            Actors: <span>{movieInfo?.Actors}</span>
+                        </MovieInfo>
+                        <MovieInfo>
+                            Plot: <span>{movieInfo?.Plot}</span>
+                        </MovieInfo>
+                    </InfoColumn>
+                    <Close onClick={() => props.onMovieSelect()}>X</Close>
+                </>
+            ) : null}
+        </Container>
+    );
 }
 
 export default MovieInfoComponent;
+
